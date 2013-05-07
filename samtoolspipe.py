71a88ef3ecfa4bb56414ae9aac07866f3dab03d9
Bing Wang <toaya.kase@gmail.com>
2013-05-07 12:06:00 -0500

    add samtoolspipe.py

---
+++ samtoolspipe.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+#coding: utf-8 
+
+#This program could runs samtools pipe on samfile
+#This program needs "samtools"
+"http://samtools.sourceforge.net/"
+
+import os,sys
+
+def print_usage():
+    print "\nUsage:\n"
+    print "$./samtoolspipe samfile"
+
+try:
+    assert len(sys.argv) == 2
+    assert sys.argv[1].endswith(".sam")
+except:
+    print_usage()
+
+#convert samfile to bam file
+sam_file = sys.argv[1]
+bam_file = sam_file.replace(".sam",".bam")
+cmd = "samtools view -bS %s > %s"%(sam_file,bam_file)
+os.system(cmd)
+#sort bamfile
+sorted_file = sam_file.replace(".sam",".sorted")
+cmd = "samtools sort %s %s"%(sam_file,sorted_file)
+#build index for bamfile
+os.system(cmd)
+cmd = "samtools index %s"%(sorted_file)
